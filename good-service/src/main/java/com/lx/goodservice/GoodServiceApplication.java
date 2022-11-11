32e4a419e0719a310ee7681b2a414f36535f02ee
李新 <763819849@qq.com>
2022-11-11 21:02:31 +0800

    [refactor] mybatis plus 测试

--- good-service/src/main/java/com/lx/goodservice/GoodServiceApplication.java
+++ good-service/src/main/java/com/lx/goodservice/GoodServiceApplication.java
@@ -1,9 +1,11 @@
 package com.lx.goodservice;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.lx.goodservice.dao")
 public class GoodServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(GoodServiceApplication.class, args);
---
+++ good-service/src/test/java/com/lx/goodservice/dao/GoodPriceDaoTest.java
@@ -0,0 +1,37 @@
+package com.lx.goodservice.dao;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.lx.goodservice.pojo.GoodPrice;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+import java.util.List;
+
+@SpringBootTest()
+@RunWith(SpringRunner.class)
+@Slf4j
+public class GoodPriceDaoTest {
+    @Autowired
+    GoodPriceDao goodPriceDao;
+
+    @Test
+    public void insert() {
+//        goodPriceDao.insert(GoodPrice.builder()
+//                .id("3")
+//                .price(10)
+//                .isNew(false)
+//                .updateTime(new Date())
+//                .build());
+    }
+    @Test
+    public void select(){
+        QueryWrapper<GoodPrice> queryWrapper = new QueryWrapper<>();
+        List<GoodPrice> goodPrices = goodPriceDao.selectList(queryWrapper);
+        goodPrices.forEach(System.out::println);
+    }
+}
