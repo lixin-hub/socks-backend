c595996627b29c756e654950bb9657b26594e516
李新 <763819849@qq.com>
2022-11-11 21:21:02 +0800

    refactor(pom):依赖优化

--- good-service/src/main/java/com/lx/goodservice/controller/GoodController.java
+++ good-service/src/main/java/com/lx/goodservice/controller/GoodController.java
@@ -4,6 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *@author LIXIN
+ *@description 商品相关api
+ *@date 2022/11/11 21:11
+ */
 @RestController
 @RequestMapping("good")
 @Slf4j
