97962cff4b42f292999ef0430252f29c44d104ba
李新 <763819849@qq.com>
2022-11-11 17:12:15 +0800

    [feat] 增加gateway服务

---
+++ spring-gateway/src/main/java/com/lx/SpringBootGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.lx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGatewayApplication.class, args);
+    }
+
+}
