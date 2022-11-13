6e7405ea65f5f7da785921d19a534246b8522cd0
李新 <763819849@qq.com>
2022-11-13 16:33:15 +0800

    feat(profile):设置ribbon刷新时间为3000ms

--- spring-gateway/src/main/java/com/lx/SpringBootGatewayApplication.java
+++ spring-gateway/src/main/java/com/lx/SpringBootGatewayApplication.java
@@ -1,12 +1,18 @@
 package com.lx;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootGatewayApplication.class, args);
     }
-
+    @Bean
+    public IRule rule(){
+        return new RandomRule();
+    }
 }
